d3fc64dd41bafd465528e5df6c3ef5c2db708da3
Ewa Janczykowska <janczykowska@student.agh.edu.pl>
2020-03-01 14:34:08 +0100

    Losowe slowa

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -52,7 +52,57 @@ public class Hangman {
                     " / \\  |\n" +
                     "      |\n" +
                     "========"
-    };
+    }
+    
+// public void runGame() {
+//    	
+//    	ArrayList<String> words = new ArrayList<>();
+//    	for (int i=0; i<word.length(); i++) {
+//    		
+//    	}
+//    
+//    	Scanner scanner = new Scanner(System.in);
+//    	String letter = scanner.nextLine();
+//    	
+//    	for (Words w: words) {
+//    		if w.contains(letter) {
+//    			
+//    		}
+//    	}
+//    }
+    
+    //przypisywanie slow z pliku (z innej klasy) do Listy
+    public ArrayList<String> getListOfWords() {
+    	ArrayList<String> words = new ArrayList<>();
+    	String line = null;
+    	while((line = bufferedReader.readLine()) != null) {
+    		words.add(line);
+    	}
+    }
+    
+    //pobieranie losowego slowa z Listy
+    public String getRandomWord() {
+    	Random random = new Random();
+    	String randomIndex = random.nextInt(words.size());	
+    	String word = words.get(randomIndex);
+    	return word;
+    }
+    
+    //ile liter ma slowo
+    public int countLeters() {
+    	return word.length();
+    }
+    
+    //przypisanie kresek do dlugosci slowa
+    public void printDashes() {
+    	for (int i=0; i<word.length(); i++) {
+    		System.out.println("_");
+    	}
+    }
+
+    
+    //public booleam checkLetter() {	
+    //}
 
     public static void main(String[] args) {
     }
