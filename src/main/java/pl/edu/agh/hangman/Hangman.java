6fe4e4e7da516ba542837a6ff77d9d7b51ea786b
Magdalena Gadek-Krzywodajc <magdalenagadek2012@gmail.com>
2020-03-01 14:08:59 +0100

    OdczytSlow

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,8 +1,41 @@
 package pl.edu.agh.hangman;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+
+
 
 public class Hangman {
+	
+	//String filePath = "C:\\Users\\student14\\Desktop\\Hangman\\java-hangman\\src\\main\\resources.Slowa.txt";
+	
+	private static FileReader fileReader;
+	private static BufferedReader bufferedReader;
+	
+	
+	public void readFile() throws IOException{
+		try {
+			File file = new File ("C:\\Users\\student14\\Desktop\\Hangman\\java-hangman\\src\\main\\resourcesSlowa.txt");		
+			fileReader = new FileReader(file);
+			bufferedReader = new BufferedReader(fileReader);
+			String line = null;
+			line = bufferedReader.readLine();
+			}
+			
+		catch (IOException e) {
+	        System.out.println("BŁĄD ODCZYTU Z PLIKU!");
+		}
+	}
+	
+	
+	
+	
 
     public static final String[] HANGMANPICS = new String[]{
+    		
             "  +---+\n" +
                     "  |   |\n" +
                     "      |\n" +
